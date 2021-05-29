317b227a1092edf4027389d51c1c17095dddef3f
previzdaniel <dani.2000.2x@gmail.com>
2021-05-29 23:13:20 +0200

    Add project files.

---
+++ Anna/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Anna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(188, 246);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(66, 264);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(75, 23);
+            this.btnSpeak.TabIndex = 1;
+            this.btnSpeak.Text = "Speak";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 301);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Personal assistant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSpeak;
+    }
+}
+
---
+++ Anna/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.IO;
+using System.Media;
+using System.Diagnostics;
+
+namespace Anna
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        SpeechRecognitionEngine sr = new SpeechRecognitionEngine();
+        PromptBuilder pb = new PromptBuilder();
+
+
+        public bool wake;
+
+        public bool paused;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Say("welcome");
+        }
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            s.SelectVoiceByHints(VoiceGender.Female);
+            Choices list = new Choices();
+            list.Add(File.ReadAllLines("commands.txt"));
+
+            Grammar gm = new Grammar(new GrammarBuilder(list));
+
+            try
+            {
+                sr.RequestRecognizerUpdate();
+                sr.LoadGrammar(gm);
+                sr.SpeechRecognized += Sr_SpeechRecognized;
+                sr.SetInputToDefaultAudioDevice();
+                sr.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                richTextBox1.Text += "Again";
+                return;
+            }
+
+            pb.ClearContent();
+            pb.AppendText(richTextBox1.Text);
+            s.Speak(pb);
+        }
+
+        public void Say(string phrase)
+        {
+            s.SpeakAsync(phrase);
+            wake = false;   
+        }
+
+        private void Sr_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speechSaid = e.Result.Text;
+
+            if (speechSaid == "hey anna")
+            {
+                SoundPlayer player = new SoundPlayer(@"C:\Users\dani2\source\repos\Anna\Anna\bin\Debug\listening.wav");
+                player.Play();
+                wake = true;
+            }
+
+            if (wake == true)
+            {
+                paused = false;
+
+                switch (speechSaid)
+                {
+                    case ("hello"):
+                        Say("hi");
+                        break;
+
+                    case ("how are you doing"):
+                        Say("good, how about you");
+                        break;
+
+                    case ("good"):
+                        Say("i am glad to hear it");
+                        break;
+
+                    case ("open google"):
+                        Say("opening google");
+                        Process.Start("https://www.google.com");
+                        break;
+
+                    case ("exit"):
+                        s.Speak("closing program, have a good day, sir");
+                        SendKeys.Send("%{f4}");
+                        break;
+
+                    case ("open youtube"):
+                        Say("opening youtube");
+                        Process.Start("https://www.youtube.com");
+                        break;
+
+                    case ("close tab"):
+                        Say("closing tab");
+                        SendKeys.Send("^W");
+                        break;
+
+                    case ("new tab"):
+                        Say("opening new tab");
+                        SendKeys.Send("^t");
+                        break;
+
+
+                    //4*\t + enter
+                    case ("searching"):
+                        Say("searching in youtube");
+                        SendKeys.Send("\t");
+                        SendKeys.Send("\t");
+                        SendKeys.Send("\t");
+                        SendKeys.Send("\t");
+                        SendKeys.Send("\t");
+                        SendKeys.Send("\t");
+                        SendKeys.Send("{ENTER}");
+                        break;
+
+
+                    #region play/pause
+                    //case ("pause"):
+                    //    IsPaused();
+                    //    break;
+
+                    //case ("play"):
+                    //    IsPlayed();
+                    //    break;
+                    #endregion
+
+                    case ("motherfucker"):
+                        Say("i am sorry, sir");
+                        break;
+
+
+                    case ("open notepad"):
+                        Say("opening notepad");
+                        Process.Start("notepad.exe");
+                        break;
+                }
+            }
+        }
+
+        private void IsPlayed()
+        {
+            if (paused == true)
+            {
+                paused = false;
+                Say("playing video");
+                SendKeys.Send("k");
+            }
+            else
+            {
+                Say("it's on");
+            }
+        }
+
+        private void IsPaused()
+        {
+            if (paused == false)
+            {
+                paused = true;
+                Say("pausing video");
+                SendKeys.Send("k");
+            }
+            else
+            {
+                Say("it is already paused");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
